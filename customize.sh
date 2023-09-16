4ae18afdbf40ccb581210cc041b4bfa57b408abf
tmpaxx <57354291+tmpaxx@users.noreply.github.com>
2023-09-17 03:52:48 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,7 +6,9 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/10.0.0.1/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.$((addr_offset++)).1/10.0.$((addr_offset++)).1/g' package/base-files/files/bin/config_generate
+sed -i 's/255.255.255.0/255.255.0.0/g' package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
